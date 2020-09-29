9fba0660e26c10aa6bc39d6d91a1c12f7197a5ae
Evandro Souza <evandro.souza@corp.globo.com>
2020-09-29 16:16:17 -0300

    chore: setup for swagger documentation

--- app/healthcheck/handler.go
+++ app/healthcheck/handler.go
@@ -18,10 +18,16 @@ func NewHandler() *Handler {
 	}
 }
 
+// IsOnline godoc
+// @Summary IsOnline
+// @Description Returns true or false, depeding on the state of app.
+// @Success 200 {integer} string "WORKING"
+// @Failure 400 {integer} string "NOT WORKING"
+// @Router /api/healthcheck [get]
 // IsOnline returns if the app is WORKING or NOT WORKING
 func (h *Handler) IsOnline(c echo.Context) error {
 	if h.healthCheckService.IsWorking() {
 		return c.String(http.StatusOK, "WORKING")
 	}
-	return c.String(http.StatusOK, "NOT WORKING")
+	return c.String(http.StatusBadRequest, "NOT WORKING")
 }
--- app/main.go
+++ app/main.go
@@ -5,6 +5,12 @@ import (
 	"github.com/evandroferreiras/gopher-city-bank/app/common/log"
 )
 
+// @title Gopher City Bank API
+// @version 1.0
+
+// @contact.name Evandro souza
+// @contact.email evandroferreiras@gmail.com
+
 func main() {
 	log.Init()
 
@@ -12,5 +18,6 @@ func main() {
 	v1 := echo.Group("/api")
 	h := api.NewHandler()
 	h.Register(v1)
+	h.RegisterSwagger(echo)
 	echo.Logger.Fatal(echo.Start("127.0.0.1:8585"))
 }
