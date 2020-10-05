0b40901309e6f41d7a9263e276b58a325d0eb2a1
Evandro Souza <evandro.souza@corp.globo.com>
2020-10-05 18:19:38 -0300

    feat: added support to MySQL

--- app/model/account.go
+++ app/model/account.go
@@ -6,7 +6,7 @@ import (
 
 // Account struct to illustrate database object
 type Account struct {
-	ID        string    `bson:"_id" json:"id" gorm:"primaryKey"`
+	ID        string    `bson:"_id" json:"id" gorm:"type:varchar(36);primaryKey"`
 	Name      string    `bson:"name" json:"name"`
 	Cpf       string    `bson:"cpf" json:"cpf"`
 	Secret    string    `bson:"secret" json:"secret"`
